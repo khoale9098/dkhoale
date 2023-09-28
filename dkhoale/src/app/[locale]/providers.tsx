ec3c5248e791cc080ea04ec25a5f961c64a7ec1f
khoale9098 <khoale9098@gmail.com>
2023-09-28 23:16:06 +0700

    init project

--- dkhoale/src/app/[locale]/providers.tsx
+++ dkhoale/src/app/[locale]/providers.tsx
@@ -1,10 +1,10 @@
-"use client"
+'use client'
 
-import React from "react"
-import { Provider as WrapBalancerProvider } from "react-wrap-balancer"
+import { ReactNode } from 'react'
+import { Provider as WrapBalancerProvider } from 'react-wrap-balancer'
 
 interface ProviderLayoutProps {
-  children: React.ReactNode
+  children: ReactNode
 }
 
 export default function AppProviders({ children }: ProviderLayoutProps) {
