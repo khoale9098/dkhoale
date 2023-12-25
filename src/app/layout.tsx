65131401c20fd5a9999f3f5274b4119317be9ee4
khoale9098 <khoale9098@gmail.com>
2023-12-25 22:44:06 +0700

    init web

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 import AppProviders from './providers'
 
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 interface RootLayoutProps {
   children: React.ReactNode
@@ -19,7 +19,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <head />
-      <body className={inter.className}>
+      <body className={montserrat.className}>
         <AppProviders>{children}</AppProviders>
       </body>
     </html>
