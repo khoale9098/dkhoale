18ac41e0887aa61d9cae4d47d1a99ff5368e5ded
khoale9098 <khoale9098@gmail.com>
2023-12-26 01:16:35 +0700

    add favicon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,17 +7,6 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/shared/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      flex: {
-        '2': '2 2 0%'
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
   plugins: [],
 }
 export default config
