0ca9301d2e31ed8a9832a4e4f7d2e62bd48668ad
Khoa Le <43885032+khoale9098@users.noreply.github.com>
2023-12-26 09:37:01 +0700

    add moon (#6)
    * add favicon
    
    * moon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,14 @@ import Header from '@/components/layout/Header'
 export default function HomePage() {
   return (
     <div className="flex h-dvh flex-col">
+      <div>
+        <div className="clouds" />
+      </div>
       <Header />
       <main className="relative z-20 mt-20 flex flex-col-reverse  md:flex-row">
         <div className="relative top-16 mx-auto flex h-full  flex-1 flex-col items-start pl-7">
           <Butterfly />
-          <h1 className="font-bold text-[vh]">
+          <h1 className="text-[9vh] font-bold">
             <span>DKHOA </span>
             <span className="text-stroke">{'<LE />'}</span>
           </h1>
@@ -24,9 +27,13 @@ export default function HomePage() {
             </p>
           </div>
         </div>
-        <div className="flex-1">{/* <div>1</div> */}</div>
+        <div className="flex-1 relative ">
+          <div className='w-20 absolute z-10 top-10 right-12'>
+            <img src="/images/moon.png" alt="" />
+          </div>
+        </div>
       </main>
-      <div className="fixed inset-x-0 bottom-0">
+      <div className="fixed inset-x-0 bottom-0 z-50">
         <img src="/images/sunflower.png" alt="" className="w-full" />
       </div>
     </div>
