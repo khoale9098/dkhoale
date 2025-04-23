8fb1b49b423f273112c27980fee2ae1d7172ddfd
khoale9098 <khoale9098@gmail.com>
2025-04-23 22:58:21 +0700

    chore: fix type error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,19 @@
 import '@/styles/main.css'
 import '@/styles/butterfly.css'
 import type { Metadata } from 'next'
-import { Inconsolata, Montserrat } from 'next/font/google'
+import { Inconsolata } from 'next/font/google'
+import { PropsWithChildren } from 'react'
 
 const font = Inconsolata({
   subsets: ['latin'],
 })
-
-interface RootLayoutProps {
-  children: React.ReactNode
-  params: { locale: string }
-}
 
 export const metadata: Metadata = {
   title: 'DKhoa Le',
   description: 'Khoa Le',
 }
 
-export default async function RootLayout({ children }: RootLayoutProps) {
+export default async function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <head />
